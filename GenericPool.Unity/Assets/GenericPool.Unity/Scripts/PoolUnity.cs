607df834c03ad306f61a3550edce636eb3e454de
Andrey Danilov <danand@inbox.ru>
2019-07-06 19:24:10 +0300

    Implement Unity components pooling

--- GenericPool.Unity/Assets/GenericPool.Unity/Scripts/PoolUnity.cs
+++ GenericPool.Unity/Assets/GenericPool.Unity/Scripts/PoolUnity.cs
@@ -1,6 +1,41 @@
 ﻿using UnityEngine;
 
+using GenericPool.Core;
+
 namespace GenericPool.Unity
 {
-    public sealed class PoolUnity : MonoBehaviour { }
+    public sealed class PoolUnity : MonoBehaviour
+    {
+        private Pool<Component> pool;
+
+        void Awake()
+        {
+            pool = new Pool<Component>(
+                idSelector:             instance => instance.name.Replace("(Clone)", string.Empty).GetHashCode(),
+                instanceSelector:       Instantiate,
+                getFromPoolCallback:    instance =>
+                                        {
+                                            instance.transform.SetParent(null, true);
+                                            instance.gameObject.SetActive(true);
+
+                                        },
+                putToPoolCallback:      instance =>
+                                        {
+                                            instance.transform.SetParent(transform, false);
+                                            instance.gameObject.SetActive(false);
+                                        });
+        }
+
+        public TComponent Get<TComponent>(TComponent prefab)
+            where TComponent : Component
+        {
+            return (TComponent)pool.Get(prefab);
+        }
+
+        public void Put<TComponent>(TComponent instance)
+            where TComponent : Component
+        {
+            pool.Put(instance);
+        }
+    }
 }
---
+++ GenericPool.Unity/Assets/GenericPool.Unity/Tests/PoolUnityTests.cs
@@ -0,0 +1,92 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+
+using NUnit.Framework;
+
+using UnityEngine;
+using UnityEngine.TestTools;
+
+namespace GenericPool.Unity.Tests
+{
+    public class PoolUnityTests
+    {
+        [UnityTest]
+        public IEnumerator Get_PropertiesAreEqual()
+        {
+            var pool = new GameObject(nameof(PoolUnity)).AddComponent<PoolUnity>();
+            yield return null;
+
+            var prefab = GameObject.CreatePrimitive(PrimitiveType.Cube).transform;
+
+            prefab.name = "Cube";
+            prefab.localScale = Vector3.one * 2;
+
+            yield return null;
+
+            var instance = pool.Get(prefab);
+
+            yield return null;
+
+            Assert.AreEqual(prefab.localScale, instance.localScale);
+        }
+
+        [UnityTest]
+        public IEnumerator Put_OneObjectManyTimes()
+        {
+            var pool = new GameObject(nameof(PoolUnity)).AddComponent<PoolUnity>();
+            yield return null;
+
+            var prefab = GameObject.CreatePrimitive(PrimitiveType.Cube).transform;
+            prefab.name = "Cube";
+
+            yield return null;
+
+            var objectAmount = 10;
+
+            for (int i = 0; i < objectAmount; i++)
+            {
+                var instance = pool.Get(prefab);
+                yield return null;
+
+                pool.Put(instance);
+                yield return null;
+            }
+
+            yield return null;
+
+            Assert.AreEqual(1, pool.transform.childCount);
+        }
+
+        [UnityTest]
+        public IEnumerator Put_ManyObjectsManyTimes()
+        {
+            var pool = new GameObject(nameof(PoolUnity)).AddComponent<PoolUnity>();
+            yield return null;
+
+            var prefab = GameObject.CreatePrimitive(PrimitiveType.Cube).transform;
+            prefab.name = "Cube";
+
+            yield return null;
+
+            var objectAmount = 10;
+            var instances = new List<Transform>();
+
+            for (int i = 0; i < objectAmount; i++)
+            {
+                var instance = pool.Get(prefab);
+                instances.Add(instance);
+                yield return null;
+            }
+
+            foreach (Transform instance in instances)
+            {
+                pool.Put(instance);
+                yield return null;
+            }
+
+            yield return null;
+
+            Assert.AreEqual(objectAmount, pool.transform.childCount);
+        }
+    }
+}
