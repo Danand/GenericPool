89086b53e9eb580eaf56688ac9e70752ddf09fcb
Andrey Danilov <danand@inbox.ru>
2019-07-06 18:53:44 +0300

    Release Unity package 0.1.2

---
+++ Scripts/PoolAwaiterUnityCoroutine.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections;
+
+using GenericPool.Interfaces;
+
+using UnityEngine;
+
+namespace GenericPool.Unity
+{
+    public sealed class PoolAwaiterUnityCoroutine : IPoolAwaiter
+    {
+        private readonly PoolUnity pool;
+
+        private float delaySeconds;
+
+        public PoolAwaiterUnityCoroutine(PoolUnity pool)
+        {
+            this.pool = pool;
+        }
+
+        bool IPoolAwaiter.HasDelay
+        {
+            get { return delaySeconds > 0; }
+        }
+
+        void IPoolAwaiter.AddDelay(TimeSpan delay)
+        {
+            delaySeconds += Convert.ToSingle(delay.TotalSeconds);
+        }
+
+        void IPoolAwaiter.AddTask(Action action)
+        {
+            pool.StartCoroutine(Awaiting(action));
+        }
+
+        private IEnumerator Awaiting(Action action)
+        {
+            yield return new WaitForSeconds(delaySeconds);
+            action.Invoke();
+        }
+    }
+}
---
+++ Scripts/PoolUnity.cs
@@ -0,0 +1,49 @@
+﻿using UnityEngine;
+
+namespace GenericPool.Unity
+{
+    public sealed class PoolUnity : MonoBehaviour
+    {
+        private Pool pool;
+
+        void Awake()
+        {
+            pool = new Pool();
+
+            pool.Register<Component>(
+                idSelector:             instance => instance.name.Replace("(Clone)", string.Empty).GetHashCode(),
+                instanceSelector:       Instantiate,
+                getFromPoolCallback:    instance =>
+                                        {
+                                            instance.transform.SetParent(null, true);
+                                            instance.gameObject.SetActive(true);
+
+                                        },
+                putToPoolCallback:      instance =>
+                                        {
+                                            instance.transform.SetParent(transform, false);
+                                            instance.gameObject.SetActive(false);
+                                        });
+
+            pool.PoolAwaiter = new PoolAwaiterUnityCoroutine(this);
+        }
+
+        public TComponent Get<TComponent>(TComponent prefab)
+            where TComponent : Component
+        {
+            return pool.Get(prefab);
+        }
+
+        public PoolGetBuilder<TComponent> GetWith<TComponent>(TComponent prefab)
+            where TComponent : Component
+        {
+            return pool.GetWith(prefab);
+        }
+
+        public void Put<TComponent>(TComponent instance)
+            where TComponent : Component
+        {
+            pool.Put(instance);
+        }
+    }
+}
