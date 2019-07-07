c5a341e6b93bc3e260c7d8604a6424dce3af516d
Andrey Danilov <danand@inbox.ru>
2019-07-07 17:50:29 +0300

    Add script creating `.unitypackage`

---
+++ create_unitypackage.bat
@@ -0,0 +1,7 @@
+@title Create .unitypackage
+@echo off
+set unity_path=c:\Program Files\Unity\Hub\Editor\2019.1.7f1\Editor\Unity.exe
+set solution_path=%~dp0
+echo Creating package...
+"%unity_path%" -projectPath "%solution_path%\GenericPool.Unity" -quit -batchmode -exportPackage "Assets/GenericPool.Unity" "%solution_path%\GenericPool.Unity.unitypackage"
+echo Success.
