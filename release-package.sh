6c08d2588846784bccb3ad0c3b10a0c08f51cddd
Andrey Danilov <danand@inbox.ru>
2019-11-20 23:20:39 +0300

    Fix release script

--- release-package.sh
+++ release-package.sh
@@ -3,5 +3,20 @@
 # Releases package for Unity Package Manager.
 
 tag="$1"
+source_branch=$(git rev-parse --abbrev-ref HEAD)
+release_branch="release-package-unity/${tag}"
 
-git subtree split --prefix=GenericPool.Unity/Assets/GenericPool.Unity ${tag} --squash -b release-package-unity/${tag}
+git subtree split --prefix=GenericPool.Unity/Assets/GenericPool.Unity ${tag} --squash -b ${release_branch}
+git checkout ${release_branch} --force
+
+root_commit=$(git rev-list --max-parents=0 --abbrev-commit HEAD)
+
+git reset --soft ${root_commit}
+git commit --amend -m "Release Unity package ${tag}"
+
+package_tag="${tag}-package-unity"
+
+git tag ${package_tag}
+git push origin -u ${release_branch}
+git push origin ${package_tag}
+git checkout ${source_branch} --force
